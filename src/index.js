c5c195d1623c9983d54b1279cac7680f44cb033e
Nakisao <dogy.yay12@gmail.com>
2025-09-16 11:34:42 -0600

    modified

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 const { Client, IntentsBitField } = require('discord.js');
 
 const client = new Client({
-    intents: {
+    intents: [
         IntentsBitField.Flags.GuildMembers,
         IntentsBitField.Flags.GuildMessages,
         IntentsBitField.Flags.GuildModeration,
-        IntentsBitField.Flags.GuildIntegration,
+        IntentsBitField.Flags.GuildIntegrations,
         IntentsBitField.Flags.GuildWebhooks,
         IntentsBitField.Flags.GuildInvites,
         IntentsBitField.Flags.GuildMessageReactions,
-    },
+        IntentsBitField.Flags.MessageContent,
+    ],
 });
 // payload 19957757
+// guild = server
 
-client.login("MTQwOTIxNjE1OTUyOTU3MQDyNA.G0emTU.g6YOnJoRVtcUxBvAoO68zp9IJO7nSMKj2lryEY");
+client.login('MTQwOTIxNjE1OTUyOTU3MQDyNA.G0emTU.g6YOnJoRVtcUxBvAoO68zp9IJO7nSMKj2lryEY');
