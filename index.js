40c348a516d7734706954e2dc872fd6ee41b2cdc
Nakisao <dogy.yay12@gmail.com>
2025-10-10 12:00:26 -0600

    what

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new Client({
 
 client.cooldowns = new Collection();
 client.commands = new Collection();
-const foldersPath = path.join(__dirname, 'commands');
+const foldersPath = path.join(__dirname, 'src', 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
@@ -41,7 +41,7 @@ for (const folder of commandFolders) {
 	}
 }
 
-const eventsPath = path.join(__dirname, 'events');
+const eventsPath = path.join(__dirname, 'src','events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
 for (const file of eventFiles) {
