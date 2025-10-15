4b1e10750ee829ebcc4b9f138f0519dd18f7b638
Nakisao <dogy.yay12@gmail.com>
2025-10-15 11:57:02 -0600

    a ton of stuff i dont understand

---
+++ src/commands/isd/intel-archive.js
@@ -0,0 +1,4 @@
+// delete an intelligence file and stores it somewhere else
+// requires: user that the file is associated with (ID)
+// requires: be SC-4+ or ISD
+// delete from MongoDB (active server) and store in Github (archive server)
