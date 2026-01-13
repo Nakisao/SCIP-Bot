0e702a171a200ebac3164ba38a6e533f6990efff
Nakisao <dogy.yay12@gmail.com>
2026-01-13 11:56:14 -0700

    cool people (dont abbreviate)

---
+++ src/events/verification/find_user.js
@@ -0,0 +1,4 @@
+// essentially just a function to find a user in the verification system to return information
+// verification system should be a file with roblox & discord user ID,
+// plus their rank information and their RAISA Certificates awarded
+// should include when last modified to prevent stale data and rank tracking issues
