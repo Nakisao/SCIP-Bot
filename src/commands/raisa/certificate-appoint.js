b1afa20c16b0f9d3f0f2912f9083118120fcfe8e
Nakisao <dogy.yay12@gmail.com>
2026-01-13 11:52:23 -0700

    cool stfrf

--- src/commands/raisa/certificate-appoint.js
+++ src/commands/raisa/certificate-appoint.js
@@ -2,3 +2,4 @@
 // *requires user to be verified with the bot
 // *appointer requires RAISA Certificate role specified
 // can be a name or RAISA certificate ID to appoint
+// would rely on the verification system to find the user to appoint to
